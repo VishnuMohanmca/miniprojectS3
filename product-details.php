c87ad61b447146da3f742cb7451bad6bd3baa6d2
vishnumohan <vishnumohan2023b@mca.ajce.in>
2022-10-06 15:41:22 +0530

    first commit

---
+++ product-details.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>PHPJabbers.com | Free Mobile Store Website Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+</head>
+
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-xs-12">
+                    <ul class="left-info">
+                        <li><a href="#"><i class="fa fa-envelope"></i>mobistore@gmail.com.com</a></li>
+                        <li><a href="#"><i class="fa fa-phone"></i>7025614019</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="right-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="cart.php"><i class=""></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <header class="">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <h2>Mobile Store<em></em></h2>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="products.php">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="checkout.php">Checkout</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About</a>
+
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="about.php">About Us</a>
+                                <a class="dropdown-item" href="blog.php">Blog</a>
+                                <a class="dropdown-item" href="testimonials.php">Testimonials</a>
+                                <a class="dropdown-item" href="terms.php">Terms</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Page Content -->
+    <div class="page-heading header-text">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1><small><del><sup>Rs</sup>20000 </del></small> &nbsp; <sup>Rs</sup>17000</h1>
+                    <span>
+                Vivo Y21t
+            </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div>
+                        <img src="../mobiletemplate/images/vivo-y21-000.jpg" alt="" class="img-fluid wc-image">
+                    </div>
+
+                    <br>
+
+                    <div class="row">
+                        <div class="col-sm-4 col-6">
+                            <div>
+                                <img src="../mobiletemplate/images/vivo.jfif" alt="" class="img-fluid">
+                            </div>
+                            <br>
+                        </div>
+                        <div class="col-sm-4 col-6">
+                            <div>
+                                <img src="../mobiletemplate/images/vivo1.jfif" alt="" class="img-fluid">
+                            </div>
+                            <br>
+                        </div>
+                        <div class="col-sm-4 col-6">
+                            <div>
+                                <img src="../mobiletemplate/images/vivo2.jfif" alt="" class="img-fluid">
+                            </div>
+                            <br>
+                        </div>
+                    </div>
+
+                    <br>
+                </div>
+
+                <div class="col-md-5">
+                    <div class="sidebar-item recent-posts">
+                        <div class="sidebar-heading">
+                            <h4>Info</h4>
+                        </div>
+
+                        <div class="content">
+                            <p>Vivo Y21 The phone comes with a 6.50-inch touchscreen display offering a resolution of 720x1600 pixels. Vivo Y21 is powered by an octa-core MediaTek Helio P35 (MT6765) processor. It comes with 4GB of RAM. The Vivo Y21 runs
+                                Android 11 and is powered by a 5000mAh battery.
+                            </p>
+                        </div>
+                    </div>
+
+                    <br>
+                    <br>
+
+                    <h4>Add to Cart</h4>
+
+                    <br>
+
+                    <form action="" method="post">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <label for="">Extra 1</label>
+                                    <select class="form-control">
+                      <option value="0">Extra A</option>
+                      <option value="0">Extra B</option>
+                      <option value="0">Extra C</option>
+                      <option value="0">Extra D</option>
+                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <label for="">Quantity</label>
+                                    <input type="text" value="1" required="" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <a href="#" class="filled-button">Add to Cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                    <br>
+                </div>
+            </div>
+
+            <br>
+
+            <h4>Description</h4>
+
+            <p>The new Vivo Y21T is one of the most happening devices for be launched in recent times. Multitasking between all kinds of apps is piece of cake for the Y21T. It's powered by the Qualcomm Snapdragon 680 processor, and 4GB of RAM. The Vivo Y21T
+                has a stunning 6.58-inch display running a high-resolution of 1080 x 2408px. At a time where photos span multiple megabytes and movies even more, the Y21T comes with ample space - 128GB.Storage can be expanded up to 1TB using the microSD
+                card slot. The rear camera setup consists of an impressive 50MP sensor, alongside two others, one for macros and the other for bokehs.
+            </p>
+
+
+
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+
+    <!-- Footer Starts Here -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 footer-item">
+                    <h4>MobiStore</h4>
+                    <p></p>
+                    <ul class="social-icons">
+                        <li><a rel="nofollow" href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 footer-item">
+                    <h4>Useful Links</h4>
+                    <ul class="menu-list">
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 footer-item">
+                    <h4>Additional Pages</h4>
+                    <ul class="menu-list">
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Terms</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 footer-item last-item">
+                    <h4>Contact Us</h4>
+                    <div class="contact-form">
+                        <form id="contact footer-contact" action="" method="post">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <fieldset>
+                                        <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <fieldset>
+                                        <input name="email" type="text" class="form-control" id="email" pattern="[^ @]*@[^ @]*" placeholder="E-Mail Address" required="">
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-12">
+                                    <fieldset>
+                                        <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-12">
+                                    <fieldset>
+                                        <button type="submit" id="form-submit" class="filled-button">Send Message</button>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        Copyright © 2022 Company Name - mobistore: <a href="https://mobistore.com/">mobistore.com</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language="text/Javascript">
+        cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+        function clearField(t) { //declaring the array outside of the
+            if (!cleared[t.id]) { // function makes it static and global
+                cleared[t.id] = 1; // you could use true and false, but that's more typing
+                t.value = ''; // with more chance of typos
+                t.style.color = '#fff';
+            }
+        }
+    </script>
+
+</body>
+
+</html>
